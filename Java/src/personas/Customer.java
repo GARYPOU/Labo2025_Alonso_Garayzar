91adee0fc988a454cd3c9983e3036d9c4e32b8c7
Garyx <166631138+GARYPOU@users.noreply.github.com>
2025-04-20 12:19:38 -0300

    Create Customer.java

---
+++ Java/src/personas/Customer.java
@@ -0,0 +1,28 @@
+
+
+public class Customer
+
+{
+    private String name;
+
+
+    public Customer(string name){
+        this.name=name;
+
+
+    }
+
+
+
+    public string getName(){
+        return name;
+    }
+
+  
+        public static void main(String[] args) {
+           
+            
+
+          
+        }
+    }
