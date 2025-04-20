4eb9ec61456a9c2807b771555880c9778ce101ef
Garyx <166631138+GARYPOU@users.noreply.github.com>
2025-04-20 12:18:54 -0300

    Create Shop.java

---
+++ Java/src/objetos/Shop.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Shop
+
+{
+   
+    
+
+
+    
+
+  
+        public static void main(String[] args) {
+
+           Customer c1 = new Customer();
+           c1.name="pinky";
+            System.out.println("Hello"+c1.name);
+
+          
+        }
+    }
