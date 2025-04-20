b28820a63af404949cde202971f6e0ba2c4964f6
Garyx <166631138+GARYPOU@users.noreply.github.com>
2025-04-20 13:50:02 -0300

    Create Clothing.java

---
+++ Java/src/objetos/Clothing.java
@@ -0,0 +1,23 @@
+
+
+public class Clothing
+
+{
+   
+        private String description;
+        private double price;
+        private String size="M";
+
+
+
+    
+
+  
+        public static void main(String[] args) {
+
+          
+
+
+          
+        }
+    }
