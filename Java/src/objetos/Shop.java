e512574b9c6f2200e557808811eddfa1b11e3dac
Garyx <166631138+GARYPOU@users.noreply.github.com>
2025-04-20 13:49:39 -0300

    Update Shop.java

--- Java/src/objetos/Shop.java
+++ Java/src/objetos/Shop.java
@@ -1,5 +1,4 @@
 
-import java.util.Scanner;
 
 public class Shop
 
@@ -12,10 +11,18 @@ public class Shop
 
   
         public static void main(String[] args) {
-
+            double tax = 0.2;
+            double total = 0;
            Customer c1 = new Customer();
            c1.name="pinky";
-            System.out.println("Hello"+c1.name);
+           System.out.println("Hello"+c1.name);
+           Clothing item1 = new Clothing("Blue Jacket", 20.9, "M");
+           Clothing item2 = new Clothing("Orange T-shirt", 10.5, "S");
+           System.out.println("Iteam 1 datos:"+" "+item1.description+" "+ item1.price+ " "+item1.size);
+           System.out.println("Iteam 2 datos:"+" "+item2.description+" "+ item2.price+ " "+item2.size);
+            total = ((item2.price*2)+item1)*(1+tax);
+            System.out.println(total);
+           
 
           
         }
