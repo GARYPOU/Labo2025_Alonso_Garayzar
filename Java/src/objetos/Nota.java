76ca9c713fd0dd3cd733b5f696fbe040421c12d8
GARYPOU <tomasgarayzar@gmail.com>
2025-04-28 09:10:35 -0300

    listo

--- Java/src/objetos/Nota.java
+++ Java/src/objetos/Nota.java
@@ -2,7 +2,6 @@ package objetos;
 
 public class Nota {
     private int nota;
-    private String nombre_alumno;
     private String materia;
 
 
@@ -10,12 +9,10 @@ public class Nota {
 
     public Nota(){
         this.nota=1;
-        this.nombre_alumno="Feli";
         this.materia="matematica";
     }
-    public Nota(String nombre_alumno, int nota, String materia){
+    public Nota(int nota, String materia){
         this.nota=nota;
-        this.nombre_alumno=nombre_alumno;
         this.materia=materia;
     }
 
@@ -27,13 +24,6 @@ public class Nota {
         this.nota = nota;
     }
 
-    public String getNombre_alumno() {
-        return nombre_alumno;
-    }
-
-    public void setNombre_alumno(String nombre_alumno) {
-        this.nombre_alumno = nombre_alumno;
-    }
 
     public String getMateria() {
         return materia;
@@ -41,5 +31,11 @@ public class Nota {
 
     public void setMateria(String materia) {
         this.materia = materia;
+    }
+
+
+    public static void main(String[] args) {
+        Nota nota1 = new Nota(3,"Ingles");
+        Nota nota2 = new Nota(10,"Italiano");
     }
 }
