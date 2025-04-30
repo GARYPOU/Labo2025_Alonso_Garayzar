d87cab56ee930d5f3a94699b58c5be5fe974c4e1
GARYPOU <tomasgarayzar@gmail.com>
2025-04-30 09:07:07 -0300

    actualizacion partidos

--- Java/src/objetos/Torneo.java
+++ Java/src/objetos/Torneo.java
@@ -1,7 +1,44 @@
 package objetos;
 
+import personas.Jugador;
+
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Torneo {
-    ArrayList<Equipo>equipos;
+    private ArrayList<Equipo>equipos;
+    private ArrayList<Partido>partidos;
+
+public Torneo(){
+    if(equipos.size()<20) {
+        this.equipos = new ArrayList<>();
+    }
+}
+
+    public ArrayList<Equipo> getEquipos() {
+        return equipos;
+    }
+
+    public void setEquipos(ArrayList<Equipo> equipos) {
+        this.equipos = equipos;
+    }
+
+    public Torneo(ArrayList<Equipo>equipos) {
+    this.equipos = equipos;
+
+
+}
+public ArrayList<Partido> fixture(){
+    String VS;
+    for(int i=0; i<equipos.size(); i++){
+        for (int j=1; j<=equipos.size();i++){
+            if(Objects.equals(equipos.get(i).getHorario(), equipos.get(j + i).getHorario())){
+                VS=equipos.get(i).getNombre()+"VS"+equipos.get(j+i).getNombre();
+                partidos.add(new Partido(1,2,2000,equipos.get(i).getHorario(),VS));
+
+            }
+        }
+    }
+    return partidos;
+}
 }
--- Java/src/personas/Jugador.java
+++ Java/src/personas/Jugador.java
@@ -8,11 +8,52 @@ public class Jugador {
     private String nombre;
     private LocalDate fecha;
     private int numero;
+    private boolean capitan;
 
     public Jugador(){
         this.nombre="Gustavo";
         this.fecha = LocalDate.of(2004,12,31);
         this.numero=15;
-        
+        this.capitan=true;
+    }
+    public Jugador(String nombre, LocalDate fecha, int numero, boolean capitan){
+        this.nombre=nombre;
+        this.fecha = fecha;
+        this.numero= numero;
+        this.capitan = capitan;
+
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public boolean isCapitan() {
+        return capitan;
+    }
+
+    public void setCapitan(boolean capitan) {
+        this.capitan = capitan;
     }
 }
