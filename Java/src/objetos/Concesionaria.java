cafa17365fb6144bfa29b0b2ecd883df3f5d3cbc
GARYPOU <tomasgarayzar@gmail.com>
2025-05-19 12:42:12 -0300

    listo

--- Java/src/objetos/Concesionaria.java
+++ Java/src/objetos/Concesionaria.java
@@ -23,12 +23,33 @@ public class Concesionaria {
             int bici = 0;
             int camio = 0;
             int auto = 0;
+            if(v instanceof Bici){
+                bici=bici+1;
+            }
+            else if(v instanceof Carro){
+                auto=auto+1;
+            }
+            else if(v instanceof Camioneta){
+                camio=camio+1;
+            }
+            if(camio>bici && camio>auto){
+                System.out.println("Camioneta");
+            }
+            if(camio<bici && auto<bici){
+                System.out.println("Bicicleta");
+            }
+            if(auto>bici && auto>camio){
+                System.out.println("Auto");
+            }
 
 
         }
     }
 
     public static void main(String[] args) {
+    SistVehiculo s1 = new SistVehiculo();
+    Concesionaria c1 = new Concesionaria();
+    c1.masCant();
 
     }
 }
